451b49ced4bae78bd6a639d1b5581c73b43b2369
kif11 <kovalewskiy@gmail.com>
2020-07-11 13:16:40 -0700

    Do not loop gate rising animation

--- src/components/AnimationPlayer.js
+++ src/components/AnimationPlayer.js
@@ -3,9 +3,11 @@ const THREE = AFRAME.THREE;
 
 export default {
   schema: {
+    loop: { type: 'int', default: 1 },
   },
 
   update: function () {
+    const { loop } = this.data;
     
     this.el.addEventListener('object3dset', (event) => {
       if(event.target.object3D.children.length == 0) {
@@ -18,22 +20,32 @@ export default {
       
       // Play all clips
       this.clips.forEach((clip) => {
-        this.mixer.clipAction(clip).play();
+        const action = this.mixer.clipAction(clip);
+
+        if (loop === 0) {
+          action.setLoop(THREE.LoopOnce);
+          action.clampWhenFinished = true;
+        }
+
+        action.play();
       });
     });
   },
+
   resetAll: function() {
     this.clips.forEach((clip) => {
       this.mixer.clipAction(clip).reset();
       this.mixer.clipAction(clip).enabled = false;
     });
   },
+
   playAll: function() {
     this.clips.forEach((clip) => {
       this.mixer.clipAction(clip).play();
       this.mixer.clipAction(clip).enabled = true;
     });
   },
+
   tick: function (time, timeDelta) {
     if (this.mixer) {
       this.mixer.update(timeDelta / 1000);
