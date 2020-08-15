54175efd9d5fac9b967fb1204c1d8c0e110eecfb
Sneha Belkhale <sbelkhale@yahoo.com>
2020-08-15 13:14:49 -0400

    sdf experiment

--- src/shaders/LineComputeFrag.glsl
+++ src/shaders/LineComputeFrag.glsl
@@ -12,10 +12,54 @@ float sdSolidAngle(vec3 p, vec2 c, float ra)
   float m = length(q - c*clamp(dot(q,c),0.0,ra) );
   return max(l,m*sign(c.y*q.x-c.x*q.y));
 }
+
+float ndot( in vec2 a, in vec2 b ) { return a.x*b.x - a.y*b.y; }
+
+float sdTorus( vec3 p, vec2 t )
+{
+  vec2 q = vec2(length(p.xz)-t.x,p.y);
+  return length(q)-t.y;
+}
+
 float sdCone( vec3 p, vec2 c, float h )
 {
   float q = length(p.xz);
   return max(dot(c.xy,vec2(q,p.y)),-h-p.y);
+}
+float sdSphere( vec3 p, float s )
+{
+  return length(p)-s;
+}
+vec2 rotate(vec2 v, float a) {
+	float s = sin(a);
+	float c = cos(a);
+	mat2 m = mat2(c, -s, s, c);
+	return m * v;
+}
+float opSubtraction( float d1, float d2 ) { return max(-d1,d2); }
+
+
+
+
+float sdRhombus(vec3 p, float la, float lb, float h, float ra)
+{
+  vec3 oldP = p;
+  p.x = oldP.x;
+  p.z = p.y;
+  p.y = oldP.z;
+  // vec2 pRot = rotate(p.xz, 0.05* time);
+  // p.xz = pRot;
+  p = abs(p);
+  vec2 b = vec2(la,lb);
+  float f = clamp( (ndot(b,b-2.0*p.xz))/dot(b,b), -1.0, 1.0 );
+  vec2 q = vec2(length(p.xz-0.5*b*vec2(1.0-f,1.0+f))*sign(p.x*b.y+p.z*b.x-b.x*b.y)-ra, p.y-h);
+  return min(max(q.x,q.y),0.0) + length(max(q,0.0));
+}
+float scene(vec3 pos)
+{
+  float rhombus = sdRhombus(pos, 10.0,30.0,10.0,10.0);
+  float sphere = sdSphere(pos, 14.0);
+  return opSubtraction(sphere,rhombus);
 }
 void main() {
   vec3 prevPos;
@@ -25,8 +69,8 @@ void main() {
 
   if (vUv.x < 0.01) {
     prevPos = mainPos2.xyz;
-    float angle = time / 5.0;
-    vec3 offset = vec3(time, time, 0.0);
+    float angle = time / 15.0;
+    vec3 offset = vec3(0.5*time, 0.5*time, 0.0);
     // velocity at desired pos
     float noise0 = cnoise(0.1 * prevPos.xyz + offset + vec3(0.0, 0.0, 0.0));
     float noisex = cnoise(0.1 * prevPos.xyz + offset + vec3(0.01, 0.0, 0.0));
@@ -40,34 +84,44 @@ void main() {
     float dxRot = cos(angle) * dx - sin(angle) * dy;
     float dyRot = sin(angle) * dx + cos(angle) * dy;
 
-    vec3 testPos = prevPos;
-    testPos.x += 90.0 * dxRot;
-    testPos.y += 90.0 * dyRot;
-    testPos.z += 90.0 * dz;
-    if (sdCone(testPos, vec2(sin(1.0),cos(1.0)), 20.) > 0.0) {
+    if (scene(prevPos) > 0.0) {
       // prevPos.x -= .5*(1.0 + vUv.y) * sin(5.*time + 10.0 * vUv.y);
-      float moveTowards = 30.0 * (ceil(mod(0.8*time + 10.0 * vUv.y, 2.0)) - 1.5);
+      float moveTowards = 100.0 * (ceil(mod(0.8*time + 10.0 * vUv.y, 2.0)) - 1.5);
+      float moveTowardsY = 100.0 * (fract(0.03*time + 10.0 * vUv.y) - .5);
+      float moveTowardsZ = 10.0 * (fract(0.03*time + 10.0 * vUv.y) - .5);
       float d = moveTowards - prevPos.x;
       float s = sign(d);
-      prevPos.x += .9*(1.0 + vUv.y)*s;
-      alpha = 0.1;
+
+      float d2 = moveTowardsY - prevPos.y;
+      float s2 = sign(d2);
+
+      float d3 = moveTowardsZ - prevPos.z;
+      float s3 = sign(d3);
+
+      prevPos.x += .8*(1.0 + vUv.y)*s;
+      prevPos.y += .07*(1.0 + vUv.y)*s2;
+      prevPos.z += .01*(1.0 + vUv.y)*s3;
+    alpha = 0.1;
     }
     else{
-      prevPos.x += 90.0 * dxRot;
-      prevPos.y += 90.0 * dyRot;
-      prevPos.z += 90.0 * dz;
+      prevPos.x += 122.0 * dxRot;
+      prevPos.y += 122.0 * dyRot;
+      prevPos.z += 122.0 * dz;
     }
 
   } else if (vUv.x < 0.02) {
     prevPos = mainPos2.xyz;
     prevPos.x += 0.1;
     prevPos.z += 0.1;
+    if (scene(prevPos) > 0.0) {
+      alpha = 0.1;
+    }
   } else {
     vec2 m = vUv;
     m.x -= 2. / 99.;
     vec3 mPos = texture2D(positionTex, vUv).xyz;
     prevPos = 0.2 * texture2D(positionTex, m).xyz + 0.8 * mPos;
-    if (sdCone(prevPos, vec2(sin(1.0),cos(1.0)), 20.) > 0.0) {
+    if (scene(prevPos) > 0.0) {
       alpha = 0.1;
     }
   }
--- src/shaders/LineFrag.glsl
+++ src/shaders/LineFrag.glsl
@@ -5,6 +5,7 @@ varying vec3 vPos;
 void main() {
   // float col = mod(ceil(20.0*vUv.y),2.0);
   float col = mod(ceil(20.0*vUv.y),20.0)/20.0;
-  gl_FragColor= 1.3 * vec4(col, vUv.z, 0.4, 1.0);
+  float inside = step(0.5,vUv.z);
+  gl_FragColor= 1.3 * vec4(inside*col + 0.4*col, 0.0, 0.1, 1.0);
   @import ./FogFrag;
 }
